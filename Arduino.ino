e2d0baab15e2d591c8034cc0422c41c1e18f41cd
Alex Castillo <aclex180@gmail.com>
2022-09-30 16:53:44 -0500

    Added Dc motor Functionality

--- Arduino.ino
+++ Arduino.ino
@@ -10,6 +10,9 @@ QMC5883LCompass compass;
 
 Servo microServo; //Create Servo object
 Servo Servo1; // Create Servo object
+// functional connections
+#define MOTOR_A_PWM 27 // Motor B PWM Speed
+#define MOTOR_A_DIR 14 // Motor B Direction
 
 int pos = 0; // Position of Servo
 
@@ -19,26 +22,31 @@ void setup(){
   Serial2.begin(9600);
   compass.init();   //initialize QMC5883LCompass
   delay(3000);
-
-////
-//Below is where the pins are attached to the specic servo motor this allows us
-//to control via a PWM
-
-
   //Atach servos to pins
   microServo.attach(25);
   Servo1.attach(26);
+  compass.setCalibration(-1943, 0, -197, 1268, -1145, 31);
 }
 
 
 void loop() {
-  int x, y , z, dy, dx, d; //Read QMC5883LCompass values
+  int x, y , z, dy, dx, d, azimuth; //Read QMC5883LCompass values
   compass.read();
 
   //Return XYZ readings
   x = compass.getX();
   y = compass.getY();
   z = compass.getZ();
+  azimuth = compass.getAzimuth();
+  char myArray[3];
+  compass.getDirection(myArray, azimuth);
+  Serial.print(myArray[0]);
+  Serial.print(myArray[1]);
+  Serial.print(myArray[2]);
+  Serial.println();
+  Serial.print(x);
+  Serial.print(y);
+  Serial.print(z);
 
   dx = (x*.48828125);
   dy = (y*.48828125);
@@ -69,16 +77,13 @@ void loop() {
     }
 
   }
-
+/*
   if (millis() > 5000 && gps.charsProcessed() < 10) {
-
     Serial.println(F("No GPS detected: check wiring."));
-
     while (true);
-
   }
-
-    //Movement of servo motors
+*/
+  //Movement of servo motors
   for (pos = 0; pos <= 180; pos += 1){
     microServo.write(pos);
     Servo1.write(pos);
@@ -90,30 +95,25 @@ void loop() {
    Servo1.write(pos);
    delay(15);
  }
-
- /*runDCMotor();
-  */
+ 
+ runDCMotor();
 }
-/*
+
 
 void runDCMotor(){
-
   Serial.println( "Forward..." );
   // always stop motors briefly before abrupt changes
-  digitalWrite( MOTOR_B_DIR, LOW );
-  digitalWrite( MOTOR_B_PWM, LOW );
-  delay( DIR_DELAY );
+  digitalWrite( MOTOR_A_DIR, LOW );
+  digitalWrite( MOTOR_A_PWM, LOW );
+  delay(1000);
   // set the motor speed and direction
-  digitalWrite( MOTOR_B_DIR, HIGH ); // direction = forward
-  analogWrite( MOTOR_B_PWM, 255-PWM_SLOW ); // PWM speed = slow
-  isValidInput = true;
+  digitalWrite( MOTOR_A_DIR, HIGH ); // direction = forward
+  analogWrite( MOTOR_A_PWM, 50 ); // PWM speed = slow
 
   delay(10000);
 
   // 3) Soft stop (preferred)
   Serial.println( "Soft stop (coast)..." );
-  digitalWrite( MOTOR_B_DIR, LOW );
-  digitalWrite( MOTOR_B_PWM, LOW );
-  isValidInput = true;
-
-}*/
+  digitalWrite( MOTOR_A_DIR, LOW );
+  digitalWrite( MOTOR_A_PWM, LOW );
+}
