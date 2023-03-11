77550471d848941851326f726cb877cfe6f0be54
Alex Castillo <aclex180@gmail.com>
2023-03-11 15:27:17 -0600

    Finished adding all functions, frontend has not been worked on. Need to add looping through of all waypoints talk to frontend developer about

--- ArduinoCode/MyDuckClass.cpp
+++ ArduinoCode/MyDuckClass.cpp
@@ -1,269 +1,177 @@
 #include "Arduino.h"
-#include <math.h>       /* atan2 */
+#include <math.h> /* atan2 */
 #include "MyDuckClass.h"
 #include <ESP32Servo.h>
 #include <analogWrite.h>
 #include <ESP32Tone.h>
 #include <ESP32PWM.h>
 #include <TinyGPSPlus.h>
-TinyGPSPlus gps;
 #include <QMC5883LCompass.h>
-QMC5883LCompass compass;
 
 
+MyDuckClass::MyDuckClass(int pinPropellorSpeed, int pinPropellorDirection, int pinHeadingMotor, int pinHeadBobbing, TinyGPSPlus LocationGps, QMC5883LCompass HeadingCompass)
+  : _pinPropSpeed(pinPropellorSpeed),
+    _pinPropDirection(pinPropellorDirection),
+    _pinHM(pinHeadingMotor),
+    _pinHB(pinHeadBobbing),
+    _GPS(LocationGps),
+    _HeadingCompass(HeadingCompass),
+    headBobbingServo(),
+    PropellorServo() {
+  headBobbingServo.attach(_pinHB);
+  PropellorServo.attach(_pinHM);
+}
 
-MyDuckClass::MyDuckClass(int pinPropellorSpeed, int pinPropellorDirection, int pinHeadingMotor, int pinHeadBobbing) {
-    _pinPropSpeed = pinPropellorSpeed;
-    _pinPropDirection = pinPropellorDirection;
-    _pinHM = pinHeadingMotor;
-    _pinHB = pinHeadBobbing;
-    //Remaining Private Variables 
-    //_angleOfHeading
-    //wayPoints[10]
-    //_spotLockLocation
-    Servo headBobbingServo;
-    Servo PropellorServo;
-    headBobbingServo.attach(_pinHB);
-    PropellorServo.attach(_pinHM);
+String MyDuckClass::calibrationOfMagnotometer() {
+  //Used to calibrate heading (IMPORTANT)
+  Servo PropellorServo;
+  //Turn heading to face perpendicular to duck decoy
+  PropellorServo.write(270);
+  digitalWrite(_pinPropDirection, HIGH);  // direction = forward
+  analogWrite(_pinPropSpeed, -255);       // PWM speed = fast max is 255
+  delay(3000);                            // Delay for 3 seconds to allow propper heading of propellor
 
+  int calibrationData[3][2];
+  bool changed = false;
+  bool done = false;
+  int t = 0;
+  int c = 0;
 
-}   
+  while (done != true) {
 
-String MyDuckClass::calibrationOfMagnotometer(){
-//Used to calibrate heading (IMPORTANT)
-    Servo PropellorServo;
-    PropellorServo.attach(_pinHM);
-    //Turn heading to face perpendicular to duck decoy
-    PropellorServo.write(270);
-    digitalWrite(_pinPropDirection, HIGH); // direction = forward
-    analogWrite(_pinPropSpeed, -255); // PWM speed = fast max is 255
-    delay(3000); // Delay for 3 seconds to allow propper heading of propellor 
-    QMC5883LCompass compass;
+    int x, y, z;
+    // Read compass values
+    _HeadingCompass.read();
+    // Return XYZ readings
+    x = _HeadingCompass.getX();
+    y = _HeadingCompass.getY();
+    z = _HeadingCompass.getZ();
+    changed = false;
 
-    int calibrationData[3][2];
-    bool changed = false;
-    bool done = false;
-    int t = 0;
-    int c = 0;
-
-    while (done != true) {
-
-        int x, y, z;
-        // Read compass values
-        compass.read();
-        // Return XYZ readings
-        x = compass.getX();
-        y = compass.getY();
-        z = compass.getZ();
-        changed = false;
-
-        if (x < calibrationData[0][0]) {
-            calibrationData[0][0] = x;
-            changed = true;
-        }
-        if (x > calibrationData[0][1]) {
-            calibrationData[0][1] = x;
-            changed = true;
-        }
-
-        if (y < calibrationData[1][0]) {
-            calibrationData[1][0] = y;
-            changed = true;
-        }
-        if (y > calibrationData[1][1]) {
-            calibrationData[1][1] = y;
-            changed = true;
-        }
-
-        if (z < calibrationData[2][0]) {
-            calibrationData[2][0] = z;
-            changed = true;
-        }
-        if (z > calibrationData[2][1]) {
-            calibrationData[2][1] = z;
-            changed = true;
-        }
-
-        if (changed && !done) {
- 
-            c = millis();
-        }
-        t = millis();
-
-
-        if ((t - c > 5000) && !done) {
-            done = true;
-
-            String result = "compass.setCalibration(";
-                        
-            result = result + calibrationData[0][0] + "," + calibrationData[0][1] + "," + calibrationData[1][0] + "," + calibrationData[1][1] + "," + calibrationData[2][0] + "," + calibrationData[2][1] + ");";
-                return result;
-        }
-
+    if (x < calibrationData[0][0]) {
+      calibrationData[0][0] = x;
+      changed = true;
+    }
+    if (x > calibrationData[0][1]) {
+      calibrationData[0][1] = x;
+      changed = true;
     }
 
+    if (y < calibrationData[1][0]) {
+      calibrationData[1][0] = y;
+      changed = true;
+    }
+    if (y > calibrationData[1][1]) {
+      calibrationData[1][1] = y;
+      changed = true;
+    }
 
+    if (z < calibrationData[2][0]) {
+      calibrationData[2][0] = z;
+      changed = true;
+    }
+    if (z > calibrationData[2][1]) {
+      calibrationData[2][1] = z;
+      changed = true;
+    }
+
+    if (changed && !done) {
+
+      c = millis();
+    }
+    t = millis();
+
+
+    if ((t - c > 5000) && !done) {
+      done = true;
+
+      String result = "_HeadingCompass.setCalibration(";
+
+      result = result + calibrationData[0][0] + "," + calibrationData[0][1] + "," + calibrationData[1][0] + "," + calibrationData[1][1] + "," + calibrationData[2][0] + "," + calibrationData[2][1] + ");";
+      return result;
+    }
+  }
 }
 
 
-//turn on propellor
-void MyDuckClass::turnOnProppellor(int propellorSpeed, int propellorDirection){
-// Turning on DC motor to have Propellor run 
+void MyDuckClass::togglePropellor(bool isOn) {
 
-#define MOTOR_A_PWM propellorSpeed // Motor B PWM Speed
-#define MOTOR_A_DIR propellorDirection // Motor B Direction
-
-
-    digitalWrite(propellorDirection, HIGH); // direction = forward
-    analogWrite(propellorSpeed, 50); // PWM speed = slow
-
-
-
-}
-
-//Turn off propellor
-void MyDuckClass::turnOffProppellor(int propellorSpeed, int propellorDirection) {
-    // Turning on DC motor to have Propellor run 
-
-#define MOTOR_A_PWM propellorSpeed // Motor B PWM Speed
-#define MOTOR_A_DIR propellorDirection // Motor B Direction
-
-
-    digitalWrite(MOTOR_A_DIR, LOW);
-    digitalWrite(MOTOR_A_PWM, LOW);
-
-
-
+  if (isOn) {
+    // Motor is turned on
+    digitalWrite(_pinPropDirection, HIGH);
+    analogWrite(_pinPropSpeed, 50);
+  } else {
+    // Motor is turned off
+    digitalWrite(_pinPropDirection, LOW);
+    digitalWrite(_pinPropSpeed, LOW);
+  }
 }
 
 
 //Change heading of the propellor motor using the servo motor
-void MyDuckClass::movePropHeading(int angleOfHeading){
-//Set direction of Propellor
-    Servo headBobbingServo;
-    headBobbingServo.attach(_pinHM);
-
+void MyDuckClass::movePropHeading(int angleOfHeading) {
+  //Set direction of Propellor
   headBobbingServo.write(angleOfHeading);
 }
 
-void MyDuckClass::spotLock(float spotLockLocation[2]) {
-// Spot lock for duck
-    float *CL;
-    CL = currentLocation();
-
-    if (distanceToWaypoint(CL, spotLockLocation) > 4) {
-        float desiredAngle = angleToWaypoint(CL, spotLockLocation);
-        //Fix angle once motor is mounted
-        movePropHeading(desiredAngle);
-    }
-
-
-}
-
-float * MyDuckClass::currentLocation(){
-//Get the current location of the duck
-
-    float Cordinates[2];
-
-    Serial2.begin(9600);
-    TinyGPSPlus gps;
-    if ((Serial2.available() > 0)) {
-        
-        if ((gps.encode(Serial2.read()))) {
-            if (gps.location.isValid()) {
-
-                Cordinates[0] = gps.location.lat();
-                
-                Cordinates[1] = gps.location.lng();
-
-            }
-            else {
-
-                Serial.print(F("INVALID"));
-
-            }
-        }
-
-    }
-    return Cordinates;
-}
 
 float MyDuckClass::GetDuckHeading() {
-    int x, y, z, dy, dx, d, azimuth; //Read QMC5883LCompass values
-    QMC5883LCompass compass;
-    compass.read();
+  int x, y, z, dy, dx, d, azimuth;  //Read QMC5883LCompass values
 
-    //Return XYZ readings
-    x = compass.getX();
-    y = compass.getY();
-    z = compass.getZ();
-    azimuth = compass.getAzimuth();
-    char myArray[3];
-    compass.getDirection(myArray, azimuth);
-    Serial.print(myArray[0]);
-    Serial.print(myArray[1]);
-    Serial.print(myArray[2]);
-    Serial.println();
-    Serial.print(x);
-    Serial.print(y);
-    Serial.print(z);
+  _HeadingCompass.read();
 
-    dx = (x * .48828125);
-    dy = (y * .48828125);
+  //Return XYZ readings
+  x = _HeadingCompass.getX();
+  y = _HeadingCompass.getY();
+  z = _HeadingCompass.getZ();
+  azimuth = _HeadingCompass.getAzimuth();
+  char myArray[3];
+  _HeadingCompass.getDirection(myArray, azimuth);
+  Serial.print(myArray[0]);
+  Serial.print(myArray[1]);
+  Serial.print(myArray[2]);
+  Serial.println();
+  Serial.print(x);
+  Serial.print(y);
+  Serial.print(z);
 
-    d = atan(dy / dx) * (180 / 3.14);
-    float result = d;
+  dx = (x * .48828125);
+  dy = (y * .48828125);
 
-    return result;
+  d = atan(dy / dx) * (180 / 3.14);
+  float result = d;
+
+  return result;
 }
 
-float MyDuckClass::angleToWaypoint (float waypointLatLonCur[2], float waypointLatLon[2]){
-    // Returns angle needed to get to waypoint
-    // cordinates of current location
-    float lat1, lon1;
-    //cordintes of desitnation
-    float lat2, lon2;
-    lat1 = waypointLatLonCur[0];
-    lon1 = waypointLatLonCur[1];
+void MyDuckClass::navigateToLocation(double targetLat, double targetLng) {
+  // Get current location and heading
+  double currentLat = _GPS.location.lat();
+  double currentLng = _GPS.location.lng();
+  double currentHeading = GetDuckHeading();
 
-    lat2 = waypointLatLon[0];
-    lon2 = waypointLatLon[1];
+  // Calculate distance and bearing to target location
+  double distance = TinyGPSPlus::distanceBetween(currentLat, currentLng, targetLat, targetLng);
+  double bearing = TinyGPSPlus::courseTo(currentLat, currentLng, targetLat, targetLng);
 
-    float temp = lon2 - lon1;
+  // Turn towards target location
+  double headingError = bearing - currentHeading;
+  if (headingError > 180) {
+    headingError -= 360;
+  } else if (headingError < -180) {
+    headingError += 360;
+  }
+  int servoPosition = map(headingError, -180, 180, 0, 180);
+  PropellorServo.write(servoPosition);
 
-    float X = cos(lat2 * 3.14 / 180) * sin(temp * 3.14 / 180);
-
-    float Y1 = (cos(lat1 * 3.14 / 180) * sin(lat2 * 3.14 / 180));
-    float Y2 = (sin(lat1 * 3.14 / 180) * cos(lat2 * 3.14 / 180) * cos(temp * 3.14 / 180));
-    float Y = Y1 - Y2;
-
-    float result = atan2(X, Y) * 180 / 3.14;
-    //bearing North, ex: 0 = N, 90 = E, 180 = S, 270 = W
-
-    return result;
-}
-
-
-float MyDuckClass::distanceToWaypoint(float waypointLatLonCur[2], float desiredpointLatLon[2]) {
-    float lat1, lon1;
-    //cordintes of desitnation
-    float lat2, lon2;
-    lat1 = waypointLatLonCur[0];
-    lon1 = waypointLatLonCur[1];
-
-    lat2 = desiredpointLatLon[0];
-    lon2 = desiredpointLatLon[1];
-
-    float p = 0.017453292519943295;    // Math.PI / 180
-
-    float a = 0.5 - cos((lat2 - lat1) * p) / 2 +
-        cos(lat1 * p) * cos(lat2 * p) *
-        (1 - cos((lon2 - lon1) * p)) / 2;
-
-    //std::cout << 12742000 * asin(sqrt(a)); // m
-
-    float result = 12742000 * asin(sqrt(a)); // m
-    
-
-    return result;
+  // Go towards target location
+  togglePropellor(true);
+  //digitalWrite(_motorPin, HIGH);
+  while (distance > 3.0) {
+    currentLat = _GPS.location.lat();
+    currentLng = _GPS.location.lng();
+    distance = TinyGPSPlus::distanceBetween(currentLat, currentLng, targetLat, targetLng);
+  }
+  //digitalWrite(_motorPin, LOW);
+  togglePropellor(false);
 }
--- ArduinoCode/MyDuckClass.h
+++ ArduinoCode/MyDuckClass.h
@@ -1,41 +1,35 @@
 #ifndef MyDuckClass_h
 #define MyDuckClass_h
 #include "Arduino.h"
-
+#include <TinyGPSPlus.h>
+#include <QMC5883LCompass.h>
+#include <ESP32Servo.h>
 
 class MyDuckClass {
-    public:
-        MyDuckClass(int pinPropellor,int pinPropDirection, int pinHeadingMotor, int pinHeadBobbing);
-            //Used to calibrate heading (IMPORTANT)
-            String calibrationOfMagnotometer();
-            // Turning on DC motor to have Propellor run
-            void turnOnProppellor(int propellorSpeed, int propellorDirection);
+public:
+  MyDuckClass(int pinPropellor, int pinPropDirection, int pinHeadingMotor, int pinHeadBobbing, TinyGPSPlus LocationGps, QMC5883LCompass HeadingCompass);
+  //Used to calibrate heading (IMPORTANT)
+  String calibrationOfMagnotometer();
+  // Turning on/off DC motor to have Propellor run isOn == true -> ON
+  void togglePropellor(bool isOn);
+  //Set direction of Propellor
+  void movePropHeading(int angleOfHeading);
+  //get heading from magnotometer
+  float GetDuckHeading();
+  void navigateToLocation(double targetLat, double targetLng);
 
-            void turnOffProppellor(int propellorSpeed, int propellorDirection);
-            //Set direction of Propellor
-            void movePropHeading(int angleOfHeading);
-            // Spot lock for duck
-            void spotLock(float spotLockLocation[2]);
-            //Get the current location of the duck
-            float * currentLocation();
-            // Returns angle needed to get to waypoint
-            float angleToWaypoint (float waypointLatLonCur[2], float waypointLatLon[2]);
-            //Get distance to specified waypoint
-            float distanceToWaypoint(float waypointLatLonCur[2], float desiredpointLatLon[2]);
-            //get heading from magnotometer
-            float GetDuckHeading() ;
 
-      
-    private:
-        //Pins for Different motors being used
-        int _pinPropSpeed,_pinPropDirection, _pinHM, _pinHB;
-        //Angle needed for heading
-        int _angleOfHeading;
-        // Array of Waypoints
-        String _wayPoints[10];
-        // Spot lock position
-        String _spotLockLocation;
-        
-
+private:
+  int _pinPropSpeed;
+  int _pinPropDirection;
+  int _pinHM;
+  int _pinHB;
+  int _angleOfHeading;
+  TinyGPSPlus _GPS;
+  QMC5883LCompass _HeadingCompass;
+  Servo headBobbingServo;
+  Servo PropellorServo;
+  String _spotLockLocation;
+    String _wayPoints[10];
 };
 #endif
